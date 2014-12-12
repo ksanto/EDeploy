08070bd5c8423298f63262079ee3d08186a56141
develop <dev@emfy.com>
2014-12-12 14:56:00 +0300

    add crud controllers

--- config/web.php
+++ config/web.php
@@ -48,7 +48,10 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.44.1'],
+    ];
 }
 
 return $config;
